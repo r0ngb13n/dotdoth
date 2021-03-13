9a66e15ad69175c7b0893deef7e3211c95d1328d
nvlh1995 <nvlh1995@gmail.com>
2021-03-13 16:04:13 +0700

    add file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -31,27 +31,8 @@
             </div>
 
             <!-- footer -->
-            <footer class="flex justify-between flex-col sm:flex-row items-center border-t mt-20 py-5">
-                <div class="flex">
-                    <div class="flex flex-shrink">
-                        <div class="flex space-x-3 items-end">
-                            <a href="#" title="facebook">
-                                <x-icons.facebook />
-                            </a>
-                            <a href="#" title="youtube">
-                                <x-icons.youtube />
-                            </a>
-                            <a href="#" title="instagram">
-                                <x-icons.instagram />
-                            </a>
-                            <a href="mailto:hai@dotdoth.com" class="text-sm text-indigo-500">hai@dotdoth.com</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="text-sm text-right flex-shrink flex-grow">
-                    <p>Crafted by <a class="text-indigo-500" href="http://github.com/bangnokia/pekyll" target="_blank">pekyll<a/> with ❤️</p>
-                </div>
-            </footer>
+            <x-footer />
+            
         </div>
     </div>
 </body>
--- resources/views/pages/danh-gia.blade.php
+++ resources/views/pages/danh-gia.blade.php
@@ -3,7 +3,7 @@
         <div class="relative max-w-7xl mx-auto">
             <div class="text-center">
                 <h2 class="text-3xl tracking-wider font-regular text-black sm:text-3xl">
-                    Đánh giá và so sánh
+                    Lmao
                 </h2>
                 <p class="text-lg mt-2 max-w-xl mx-auto text-gray-500 tracking-widest font-light sm:mt-2 sm:text-lg">
                     Các bài viết đánh giá mang tính khách quan.     
@@ -12,7 +12,9 @@
 
             <div class="mt-10 max-w-xl mx-auto grid gap-14 sm:mt-10 sm:gap-24 sm:max-w-lg lg:grid-cols-1">
                 @foreach ($posts as $post)
+                    @if($post->category == "Pha cà phê")
                     <x-blocks.blog :post="$post" />
+                    @endif
                 @endforeach
             </div>
         </div>
