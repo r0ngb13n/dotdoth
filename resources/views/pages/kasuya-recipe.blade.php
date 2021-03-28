f6b9663dd4ab7caa3f3b49dc47239abfab3aa4a0
bangnokia <bangnokia@gmail.com>
2021-03-29 01:31:42 +0700

    init table

--- resources/views/pages/kasuya-recipe.blade.php
+++ resources/views/pages/kasuya-recipe.blade.php
@@ -9,56 +9,80 @@
 </head>
 
 <body>
-    <div class="grid grid-cols-5 divide-x divide-y border border-gray-200" x-data="app()">
-        <div class="col-span-5">kasuya 4:6 recipe</div>
+    <table class="border-collapse border border-gray-200" x-data="app()">
+        <thead>
+            <tr colspan="5">
+                <th class="border">kasuya 4:6 recipe</th>
+            </tr>
+            <tr colspan="5">
+                <th class="border">Profile</th>
+            </tr>
+            <tr>
+                <tr>
+                    <th class="border" colspan="2">Dose in</th>
+                    <th class="border" colspan="3">Dose out</th>
+                </tr>
+                <tr>
+                    <th colspan="2" class="border">
+                        <input type="number" x-model="doseIn">
+                    </th>
+                    <th colspan="3" x-text="doseOut" class="border"></th>
+                </tr>
+                <tr>
+                    <th class="border">Balance</th>
+                    <th class="border">
+                        <select x-model="balance">
+                            <option value="2">Sweet</option>
+                            <option value="3">Normal</option>
+                            <option value="4">Acidity</option>
+                        </select>
+                    </th>
+                    <th class="border">Ratio</th>
+                    <th class="border">1:</th>
+                    <th class="border">
+                        <select x-model="ratio">
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                        </select>
+                    </th>
+                </tr>
+                <tr>
+                    <th class="border">Strength</th>
+                    <th class="border">
+                       <select x-model="strength">
+                        <option value="0.3">Weak</option>
+                        <option value="0.2">Balance</option>
+                        <option value="0.15">Strong</option>
+                    </select>
+                </th>
+                <th  class="border">Sweet/Acidity (40%)</th>
 
-        <div class="col-span-5">Profile</div>
+            </tr>
+            <tr>
 
-        <div class="col-span-2">Dose in</div>
-        <div class="col-span-3">Dose out</div>
+                <th class="border">TIME</th>
+                <th class="border">Target</th>
+                <th class="border">Sweet</th>
+                <th class="border">Normal</th>
+                <th class="border">Acidity</th>
+            </tr>
+        </thead>
 
-        <div class="col-span-2">
-            <input type="number" x-model="doseIn">
-        </div>
-        <div class="col-span-3" x-text="doseOut">
-        </div>
-
-        <div>Balance</div>
-        <div>
-            <select>
-                <option value="sweet">Sweet</option>
-                <option value="normal">Normal</option>
-                <option value="acidity">Acidity</option>
-            </select>
-        </div>
-        <div>Ratio</div>
-        <div>1:</div>
-        <div>
-            <select x-model="ratio">
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-            </select>
-        </div>
-
-        <div>Strength</div>
-        <div>
-             <select>
-                <option value="weak">Weak</option>
-                <option value="balance">Balance</option>
-                <option value="strong">Strong</option>
-            </select>
-        </div>
-        <div class="col-span-3">Sweet/Acidity (40%)</div>
-
-        <div>TIME</div>
-        <div>Target</div>
-        <div>Sweet</div>
-        <div>Normal</div>
-        <div>Acidity</div>
-    </div>
+        <tbody>
+            <template x-for="item in calculated" :key="item.time">
+                <tr>
+                    <td class="border" x-text="item.time"></td>
+                    <td class="border" x-text="item.target"></td>
+                    <td class="border" x-text="item.sweet"></td>
+                    <td class="border" x-text="item.normal"></td>
+                    <td class="border" x-text="item.acidity"></td>
+                </tr>
+            </template>
+        </tbody>
+    </table>
 
     <script>
         function app() {
@@ -66,12 +90,32 @@
             return {
                 doseIn: 10 + 10,
                 ratio: 15,
+                balance: 3,
+                strength: 0.2,
                 get doseOut() {
                     return this.doseIn * this.ratio;
+                },
+                get calculated() {
+                    return [
+                    {
+                        time: '00:00',
+                        target: this.doseIn * 2,
+                        sweet: '',
+                        normal: '',
+                        acidity: ''
+                    },
+                        { // first
+                            time: '00:45',
+                            target: this.doseIn * 2,
+                            sweet: '',
+                            normal: '',
+                            acidity: ''
+                        }
+                        ];
+                    }
                 }
             }
-        }
-    </script>
+        </script>
 
-</body>
-</html>
+    </body>
+    </html>
